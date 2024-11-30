e921cba036b28c30eb4aa2a4d4ee2302c8da380a
ibrahimmansur4 <ibrahimmansur4@gmail.com>
2024-11-30 22:49:15 +0500

    added ekf and ukf animation with routes

--- Kalman Filter Animation/app.py
+++ Kalman Filter Animation/app.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/ekf')
+def ekf():
+    return render_template('ekf_simulation.html')
+
+@app.route('/ukf')
+def ukf():
+    return render_template('ukf_simulation.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
